cd4e6a0b0ff72db437edccbd84c91d442f421da0
Lsc0x80 <ruqwe0@gmail.com>
2023-06-04 14:59:38 +0000

    Added updating submodules

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Setup env
 export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 mkdir out
@@ -9,10 +10,10 @@ KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-#export ARCH=arm64
-#make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android a70q_eur_open_defconfig
-#make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
+#Update submodules
+git submodule update --remote
 
+#Start build
 make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a70q_eur_open_defconfig
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
