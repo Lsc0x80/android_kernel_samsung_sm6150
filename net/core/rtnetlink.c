3b705c26812575c35cc6282226e3dce8ebbda126
Lsc0x80 <ruqwe0@gmail.com>
2023-06-06 19:01:19 +0000

    rtnetlink: Fix memory(net_device) leak when ->newlink fails

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -2733,8 +2733,9 @@ replay:
 			 */
 			if (err < 0) {
 				/* If device is not registered at all, free it now */
-				if (dev->reg_state == NETREG_UNINITIALIZED)
-					free_netdev(dev);
+				if (dev->reg_state == NETREG_UNINITIALIZED ||
+				    dev->reg_state == NETREG_UNREGISTERED)
+					    free_netdev(dev);
 				goto out;
 			}
 		} else {
